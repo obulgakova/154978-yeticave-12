d0b2b5a913d1cb067ecb447ae68b431b65169a0f
Oksana <oksbulgakova.work@gmail.com>
2021-05-31 20:17:13 +0300

    Несколько правок

--- add.php
+++ add.php
@@ -1,8 +1,9 @@
 <?php
 require 'init.php';
 
+$errors = [];
+
 if ($_SERVER ['REQUEST_METHOD'] == 'POST') {
-    $errors = [];
 
     if (!empty($_FILES['lot-img']['tmp_name'])) {
         $file_name = $_FILES['lot-img']['name'];
@@ -10,12 +11,9 @@ if ($_SERVER ['REQUEST_METHOD'] == 'POST') {
         $file_path = __DIR__ . '/uploads/';
         $file_url = '/uploads/' . $file_name;
 
-        $file_info = finfo_open(FILEINFO_MIME_TYPE);
-        $file_type = finfo_file($file_info, $tmp_name);
-        if (($file_type !== "image/jpeg") && ($file_type !== "image/png")) {
+        $file_type = strtolower(pathinfo($file_url, PATHINFO_EXTENSION));
+        if (($file_type !== "jpeg") && ($file_type !== "jpg") && ($file_type !== "png")) {
             $errors['lot-img'] = 'Загрузите картинку в формате JPG, JPEG или PNG';
-        } else {
-            move_uploaded_file($tmp_name, $file_path . $file_name);
         }
     }
 
@@ -67,40 +65,26 @@ if ($_SERVER ['REQUEST_METHOD'] == 'POST') {
         }
     }
 
-    if (count($errors) > 0) {
-        var_dump($errors);
-        $add_lot_tpl = include_template('add_lot.tpl.php', [
-            'nav_list' => $nav_list,
-            'errors' => $errors
-        ]);
-        $layout_content = include_template('layout.tpl.php', [
-            'nav_list' => $nav_list,
-            'content' => $add_lot_tpl,
-            'is_auth' => $is_auth,
-            'user_name' => $user_name,
-            'lot-name' => 'Добавление лота',
-            'title' => 'Добавление лота'
-        ]);
+    if (count($errors) <= 0) {
+        move_uploaded_file($tmp_name, $file_path . $file_name);
 
-        echo $layout_content;
-        die();
-    }
-
-    $sql = 'INSERT INTO lots (title, category_id, description, price_add, step_rate, dt_finish, img, user_id)
+        $sql = 'INSERT INTO lots (title, category_id, description, price_add, step_rate, dt_finish, img, user_id)
                 VALUES (?, ?, ?, ?, ?, ?, ?, 1)';
 
-    $stmt = db_get_prepare_stmt($db, $sql, $lot);
-    $res = mysqli_stmt_execute($stmt);
+        $stmt = db_get_prepare_stmt($db, $sql, $lot);
+        $res = mysqli_stmt_execute($stmt);
 
-    if ($res) {
-        $lot_id = mysqli_insert_id($db);
-        header("Location: lot.php?id=" . $lot_id);
+        if ($res) {
+            $lot_id = mysqli_insert_id($db);
+            header("Location: lot.php?id=" . $lot_id);
+        }
     }
 }
 
 
 $add_lot_tpl = include_template('add_lot.tpl.php', [
     'nav_list' => $nav_list,
+    'errors' => $errors
 ]);
 
 $layout_content = include_template('layout.tpl.php', [
