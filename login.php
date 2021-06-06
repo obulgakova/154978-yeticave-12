ed38ec65c6e465b9fe38b72322c22fe3272526ca
Oksana <oksbulgakova.work@gmail.com>
2021-06-06 19:09:09 +0300

    Несколько фиксов

--- login.php
+++ login.php
@@ -11,36 +11,32 @@ if ($_SERVER ['REQUEST_METHOD'] == 'POST') {
         'password'
     ];
 
-    if (!filter_var($form['email'], FILTER_VALIDATE_EMAIL)) {
-        $errors['email'] = "Введите коректный email";
-    } else {
-        $sql = 'SELECT *
-            FROM users
-            WHERE email = ?';
-
-        $stmt = $db->prepare($sql);
-        $stmt->bind_param('s', $form['email']);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $user = $result->fetch_assoc();
-
-        if (!$errors and $user) {
-            if (password_verify($form['password'], $user['password'])) {
-                $_SESSION['user'] = $user;
-                header("Location: /index.php");
-                die();
-            } else {
-                $errors['password'] = 'Вы ввели неверный пароль';
-            };
-        } else {
-            $errors['email'] = 'Такой пользователь не найден';
-        };
-    };
-
     foreach ($form as $key => $value) {
         if (in_array($key, $required_fields) && empty($value)) {
             $errors[$key] = "Заполните это поле";
-        };
+        } else if (!filter_var($form['email'], FILTER_VALIDATE_EMAIL)) {
+            $errors['email'] = "Введите коректный email";
+        } else {
+            $sql = 'SELECT *
+            FROM users
+            WHERE email = ?';
+
+            $stmt = $db->prepare($sql);
+            $stmt->bind_param('s', $form['email']);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $user = $result->fetch_assoc();
+
+            if (!$errors and $user) {
+                if (password_verify($form['password'], $user['password'])) {
+                    $_SESSION['user'] = $user;
+                } else {
+                    $errors['password'] = 'Вы ввели неверный пароль';
+                }
+            } else if (!$user) {
+                $errors['email'] = 'Такой пользователь не найден';
+            }
+        }
     };
 };
 
--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
 
-$_SESSION = [];
+unset($_SESSION['user']);
 header("Location: /");
