5f62a6400943b5a34a9d270ac327c7bccf4aa751
Oksana <oksbulgakova.work@gmail.com>
2021-06-07 16:14:50 +0300

    Поправила redirect на index.php и проверку существования юзера в БД

--- login.php
+++ login.php
@@ -1,6 +1,11 @@
 <?php
 require 'init.php';
 
+if (isset($_SESSION['user'])) {
+    http_response_code(403);
+    die();
+}
+
 $errors = [];
 
 if ($_SERVER ['REQUEST_METHOD'] == 'POST') {
@@ -43,10 +48,13 @@ if ($_SERVER ['REQUEST_METHOD'] == 'POST') {
     if (!$errors and $user) {
         if (password_verify($form['password'], $user['password'])) {
             $_SESSION['user'] = $user;
+            header("Location: /index.php");
+            die();
+
         } else {
             $errors['password'] = 'Вы ввели неверный пароль';
         }
-    } else if (!$user) {
+    } else if (!$errors and !$user) {
         $errors['email'] = 'Такой пользователь не найден';
     }
 }
@@ -55,10 +63,6 @@ if ($_SERVER ['REQUEST_METHOD'] == 'POST') {
 $login_tpl = include_template('login.tpl.php', [
     'errors' => $errors
 ]);
-if (isset($_SESSION['user'])) {
-    header("Location: /index.php");
-    die();
-}
 
 $layout_content = include_template('layout.tpl.php', [
     'content' => $login_tpl,
