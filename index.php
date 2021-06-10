6700db93e322be7ab930d12e781e8b54ee2cfae4
Oksana <oksbulgakova.work@gmail.com>
2021-06-10 18:04:42 +0300

    Полнотекстовый поиск

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ FROM lots l
          JOIN categories c ON l.category_id = c.id
 WHERE dt_finish > NOW()
 ORDER BY dt_add DESC';
+
 $result = $db->query($sql);
 $lots_list = $result->fetch_all(MYSQLI_ASSOC);
 
@@ -19,6 +20,7 @@ $main_content = include_template('main.tpl.php', [
     'nav_list' => $nav_list,
     'lots_list' => $lots_list
 ]);
+
 $layout_content = include_template('layout.tpl.php', [
     'nav_list' => $nav_list,
     'content' => $main_content,
---
+++ search.php
@@ -0,0 +1,72 @@
+<?php
+require 'init.php';
+
+$search = trim($_GET['search']) ?? '';
+$cur_page = $_GET['page'] ?? 1;
+
+$page_items = 9;
+$pages = [];
+$pages_count = 0;
+$lots_list = [];
+
+if ($search) {
+    $sql = 'SELECT COUNT(*) as cnt 
+            FROM lots 
+            WHERE dt_finish > NOW() 
+            AND MATCH(title, description)
+            AGAINST(?)';
+
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param('s', $search);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $items_count = $result->fetch_assoc()['cnt'];
+    $pages_count = ceil($items_count / $page_items);
+    $offset = ($cur_page - 1) * $page_items;
+
+    if ($pages_count > 0) {
+        $pages = range(1, $pages_count);
+    }
+
+    $sql = 'SELECT l.id, 
+           l.title,
+           l.price_add,
+           img,
+           dt_finish,
+           l.description,
+           (SELECT MAX(r.price_add) FROM rates r WHERE r.lot_id = l.id) current_price,
+           c.title                                                      category_title
+            FROM lots l 
+            JOIN categories c ON l.category_id = c.id
+            WHERE dt_finish > NOW() 
+            AND MATCH(l.title, description)
+            AGAINST(?)
+            ORDER BY dt_add ASC
+            LIMIT ?
+            OFFSET ?';
+
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param('sss', $search, $page_items, $offset);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $lots_list = $result->fetch_all(MYSQLI_ASSOC);
+}
+
+
+$search_content = include_template('search.tpl.php', [
+    'lots_list' => $lots_list,
+    'pages' => $pages,
+    'cur_page' => $cur_page,
+    'search' => $search
+]);
+
+$layout_content = include_template('layout.tpl.php', [
+    'nav_list' => $nav_list,
+    'content' => $search_content,
+    'title' => 'Поиск',
+    'search' => $search
+]);
+
+echo $layout_content;
